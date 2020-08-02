270cf5f80ff5fce7a91c12d004d34bc7b5d4c494
Masaki Kobayashi <mkobayashime@gmail.com>
2020-08-02 13:37:56 +0900

    [Update] Create the menu for Respon when the ext is installed

--- src/createContextMenu.js
+++ src/createContextMenu.js
@@ -1,1 +1,21 @@
 "use strict"
+
+import openCodeInRespon from "./methods/openCodeInRespon.js"
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "respon",
+    type: "normal",
+    contexts: ["selection"],
+    title: "Open this code in Respon",
+  })
+})
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (
+    info.menuItemId === "respon" &&
+    tab.url.includes("manaba.tsukuba.ac.jp")
+  ) {
+    openCodeInRespon(info.selectionText)
+  }
+})

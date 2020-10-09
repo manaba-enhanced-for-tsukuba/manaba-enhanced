5e40d4d2ff70bffbb5a2fd3ee3d38a9b72682e3e
Masaki Kobayashi <contact@mkobayashi.me>
2020-10-09 13:16:28 +0900

    [Update] Implement applyFilter()

--- src/methods/filterCourses.js
+++ src/methods/filterCourses.js
@@ -1,10 +1,26 @@
 "use strict"
 
 import checkLang from "./checkLang.js"
+let lang
 
 const filterCourses = () => {
-  // eslint-disable-next-line no-unused-vars
+  lang = checkLang()
+
   const moduleSelector = createModuleSelector()
+
+  chrome.storage.sync.get("filterConfigForModule", (result) => {
+    if (result.filterConfigForModule) {
+      moduleSelector.value = result.filterConfigForModule
+      applyFilter(result.filterConfigForModule)
+    }
+  })
+
+  moduleSelector.addEventListener("change", (e) => {
+    const curModuleCode = e.target.value
+    applyFilter(curModuleCode)
+
+    chrome.storage.sync.set({ filterConfigForModule: curModuleCode })
+  })
 }
 
 /**
@@ -25,8 +41,6 @@ const parseModuleCode = (moduleCode) => {
  * @return {string} "春", "Spring", etc...
  */
 const seasonCodeToText = (seasonCode) => {
-  const lang = checkLang()
-
   switch (seasonCode) {
     case "spring": {
       if (lang === "ja") {
@@ -66,8 +80,6 @@ const createModuleSelector = () => {
   ]
 
   const moduleCodeToText = (moduleCode) => {
-    const lang = checkLang()
-
     if (moduleCode === "all") {
       if (lang === "ja") {
         return "すべてのモジュール"
@@ -100,6 +112,91 @@ const createModuleSelector = () => {
   )
 
   return moduleSelector
+}
+
+const applyFilter = (moduleCode) => {
+  const coursesContainer = document.querySelector(".courselist tbody")
+  const courses = Array.from(coursesContainer.children)
+  courses.shift()
+
+  /**
+   * Parse course info string on the UI
+   * @param {string} courseInfoString Something like "秋A 水5,6" or "Spring AB Mon. 2"
+   * @return {{ season: Object.<string, boolean>, module: Array.<string>, dayOfWeek: Object.<string, boolean>, period: Array.<string>}}
+   */
+  const parseCourseInfoString = (courseInfoString) => {
+    if (lang === "ja") {
+      const result = courseInfoString.match(
+        /^([春秋]+)([abc]+)\s([月火水木金土日]+)([\d,]+)$/i
+      )
+      return {
+        season: {
+          spring: result[1].includes("春"),
+          autumn: result[1].includes("秋"),
+        },
+        module: result[2].split("").map((str) => str.toLowerCase()),
+        dayOfWeek: {
+          mon: result[3].includes("月"),
+          tue: result[3].includes("火"),
+          wed: result[3].includes("水"),
+          thu: result[3].includes("木"),
+          fri: result[3].includes("金"),
+          sat: result[3].includes("土"),
+          sun: result[3].includes("日"),
+        },
+        period: result[4].split(","),
+      }
+    } else if (lang === "en") {
+      const result = courseInfoString.match(
+        /^([Spring|Autumn]+)\s([abc]+)\s([Mon.|Tue.|Wed.|Thu.|Fri.|Sat.|Sun.|\s]+)\s([\d,]+)$/i
+      )
+      return {
+        season: {
+          spring: result[1].includes("Spring"),
+          autumn: result[1].includes("Autumn"),
+        },
+        module: result[2].split("").map((str) => str.toLowerCase()),
+        dayOfWeek: {
+          mon: result[3].includes("Mon"),
+          tue: result[3].includes("Tue"),
+          wed: result[3].includes("Wed"),
+          thu: result[3].includes("Thu"),
+          fri: result[3].includes("Fri"),
+          sat: result[3].includes("Sat"),
+          sun: result[3].includes("Sun"),
+        },
+        period: result[4].split(","),
+      }
+    }
+  }
+
+  if (moduleCode !== "all") {
+    const parsedModuleCode = parseModuleCode(moduleCode)
+
+    courses.forEach((course) => {
+      course.style.display = "table-row"
+
+      const courseInfoString = course.children[2].innerText
+      if (/^.+\s.+$/.test(courseInfoString)) {
+        const courseInfo = parseCourseInfoString(courseInfoString)
+
+        if (
+          courseInfo.season[parsedModuleCode.season] &&
+          courseInfo.module.includes(parsedModuleCode.module)
+        ) {
+          course.style.display = "table-row"
+        } else {
+          course.style.display = "none"
+        }
+      } else {
+        course.style.display = "table-row"
+      }
+    })
+  } else {
+    courses.forEach((course) => {
+      course.style.display = "table-row"
+    })
+  }
 }
 
 export default filterCourses

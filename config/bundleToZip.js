fff9d6399af1e109a1fc47b54c3509c107a20c50
Masaki Kobayashi <contact@mkobayashi.me>
2020-10-11 18:14:57 +0900

    [Chore] Use path.join instead of hard coded path

--- config/bundleToZip.js
+++ config/bundleToZip.js
@@ -4,7 +4,7 @@ const path = require("path")
 const admZip = require("adm-zip")
 const zip = new admZip()
 
-const packageVersion = require("../package.json").version
+const packageVersion = require(path.join(__dirname, "../package.json")).version
 
 zip.addLocalFolder(path.join(__dirname, "../build"))
 zip.writeZip(path.join(__dirname, `../${packageVersion}.zip`))

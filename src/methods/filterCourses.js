ad09e227471993e8a6becba11cd3f8a8cd56fc3d
Masaki Kobayashi <contact@mkobayashi.me>
2020-10-11 15:02:52 +0900

    [Chore] Change the name of a var result -> splitted

--- src/methods/filterCourses.js
+++ src/methods/filterCourses.js
@@ -143,46 +143,46 @@ const applyFilter = (moduleCode) => {
    */
   const parseCourseInfoString = (courseInfoString) => {
     if (lang === "ja") {
-      const result = courseInfoString.match(
+      const splitted = courseInfoString.match(
         /^([春秋]+)([abc]+)\s([月火水木金土日]+)([\d,]+)$/i
       )
       return {
         season: {
-          spring: result[1].includes("春"),
-          autumn: result[1].includes("秋"),
+          spring: splitted[1].includes("春"),
+          autumn: splitted[1].includes("秋"),
         },
-        module: result[2].split("").map((str) => str.toLowerCase()),
+        module: splitted[2].split("").map((str) => str.toLowerCase()),
         dayOfWeek: {
-          mon: result[3].includes("月"),
-          tue: result[3].includes("火"),
-          wed: result[3].includes("水"),
-          thu: result[3].includes("木"),
-          fri: result[3].includes("金"),
-          sat: result[3].includes("土"),
-          sun: result[3].includes("日"),
+          mon: splitted[3].includes("月"),
+          tue: splitted[3].includes("火"),
+          wed: splitted[3].includes("水"),
+          thu: splitted[3].includes("木"),
+          fri: splitted[3].includes("金"),
+          sat: splitted[3].includes("土"),
+          sun: splitted[3].includes("日"),
         },
-        period: result[4].split(","),
+        period: splitted[4].split(","),
       }
     } else if (lang === "en") {
-      const result = courseInfoString.match(
+      const splitted = courseInfoString.match(
         /^([Spring|Autumn]+)\s([abc]+)\s([Mon.|Tue.|Wed.|Thu.|Fri.|Sat.|Sun.|\s]+)\s([\d,]+)$/i
       )
       return {
         season: {
-          spring: result[1].includes("Spring"),
-          autumn: result[1].includes("Autumn"),
+          spring: splitted[1].includes("Spring"),
+          autumn: splitted[1].includes("Autumn"),
         },
-        module: result[2].split("").map((str) => str.toLowerCase()),
+        module: splitted[2].split("").map((str) => str.toLowerCase()),
         dayOfWeek: {
-          mon: result[3].includes("Mon"),
-          tue: result[3].includes("Tue"),
-          wed: result[3].includes("Wed"),
-          thu: result[3].includes("Thu"),
-          fri: result[3].includes("Fri"),
-          sat: result[3].includes("Sat"),
-          sun: result[3].includes("Sun"),
+          mon: splitted[3].includes("Mon"),
+          tue: splitted[3].includes("Tue"),
+          wed: splitted[3].includes("Wed"),
+          thu: splitted[3].includes("Thu"),
+          fri: splitted[3].includes("Fri"),
+          sat: splitted[3].includes("Sat"),
+          sun: splitted[3].includes("Sun"),
         },
-        period: result[4].split(","),
+        period: splitted[4].split(","),
       }
     }
   }

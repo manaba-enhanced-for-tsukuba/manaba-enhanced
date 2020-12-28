f776e3baf01f8a43fb2ebc4c2365615992882cc1
Masaki Kobayashi <contact@mkobayashi.me>
2020-12-28 23:05:25 +0900

    [Add] Add options.js and options.sass

--- src/options.js
+++ src/options.js
@@ -1,1 +1,32 @@
 "use strict"
+
+import "./style/options.sass"
+
+window.onload = () => {
+  const linkToShortcutsSettings = document.getElementById(
+    "link-to-shortcuts-settings"
+  )
+  if (linkToShortcutsSettings) {
+    linkToShortcutsSettings.onclick = (e) => {
+      e.preventDefault()
+      chrome.tabs.create({ url: "chrome://extensions/shortcuts" })
+      return false
+    }
+  }
+
+  Array.from(document.getElementsByClassName("checkbox-features")).map(
+    (dom) => {
+      const key = dom.id
+
+      chrome.storage.sync.get([key], (result) => {
+        if (result[key] === false) {
+          dom.checked = false
+        }
+      })
+
+      dom.addEventListener("change", (event) => {
+        chrome.storage.sync.set({ [key]: event.srcElement.checked })
+      })
+    }
+  )
+}

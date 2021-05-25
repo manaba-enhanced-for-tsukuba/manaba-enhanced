76db6781cfe0e36a51fb96a106f759e705769b6a
Masaki Kobayashi <contact@mkobayashi.me>
2021-05-26 01:38:13 +0900

    Define StorageSync type

---
+++ src/types/filterCources.ts
@@ -0,0 +1,10 @@
+export type SeasonCode = "spring" | "autumn"
+
+export type ModuleCode =
+  | "all"
+  | "spring-a"
+  | "spring-b"
+  | "spring-c"
+  | "autumn-a"
+  | "autumn-b"
+  | "autumn-c"
---
+++ src/types/storage.ts
@@ -0,0 +1,10 @@
+import type { ModuleCode } from "./filterCources"
+
+export type StorageSync = Readonly<{
+  "features-assignments-coloring": boolean
+  "features-autosave-reports": boolean
+  "features-deadline-highlighting": boolean
+  "features-remove-confirmation": boolean
+  "features-filter-courses": boolean
+  filterConfigForModule?: ModuleCode
+}>

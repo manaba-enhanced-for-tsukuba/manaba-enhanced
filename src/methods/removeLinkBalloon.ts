c7b33b73b08371e7b4047f18db07cc2ff73ee468
Masaki Kobayashi <contact@mkobayashi.me>
2021-05-26 03:38:58 +0900

    Refactor removeLinkBalloon

--- src/methods/removeLinkBalloon.ts
+++ src/methods/removeLinkBalloon.ts
@@ -11,21 +11,33 @@ const removeLinkBalloon = (): void => {
     }
   }
 
-  for (const link of Array.from(links)) {
-    if (link.href.includes("link_iframe_balloon")) {
-      const linkNew = document.createElement("a")
-      const url = unescape(link.href.substr(56))
-      linkNew.href = url
-      linkNew.innerHTML = !link.innerHTML.includes("http")
-        ? link.innerHTML
-        : urlClamp(url)
-      linkNew.target = "_blank"
-      linkNew.rel = "noopener noreferrer"
+  const hrefBalloonKeyword = "link_iframe_balloon?url="
 
-      if (link.parentElement) link.parentElement.insertBefore(linkNew, link)
-      link.remove()
+  Array.from(links).map((link) => {
+    if (link.href.includes(hrefBalloonKeyword)) {
+      const linkNew = document.createElement("a")
+
+      try {
+        const url = decodeURIComponent(
+          link.href.slice(
+            link.href.indexOf(hrefBalloonKeyword) + hrefBalloonKeyword.length
+          )
+        )
+
+        linkNew.href = url
+        linkNew.innerHTML = !link.innerHTML.includes("http")
+          ? link.innerHTML
+          : urlClamp(url)
+        linkNew.target = "_blank"
+        linkNew.rel = "noopener noreferrer"
+
+        if (link.parentElement) link.parentElement.insertBefore(linkNew, link)
+        link.remove()
+      } catch (err) {
+        console.error(err)
+      }
     }
-  }
+  })
 }
 
 export default removeLinkBalloon

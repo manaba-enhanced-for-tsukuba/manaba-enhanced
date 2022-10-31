d0efa29c869707f19072e5873a32c5f9e7b358a6
Yuzuki Arai <yudukikun5120@gmail.com>
2022-10-31 22:36:38 +0900

    chore: add downloads to permissions

--- src/manifest.ts
+++ src/manifest.ts
@@ -24,6 +24,7 @@ const generateManifest = () => {
         "contextMenus",
         "webRequest",
         "webRequestBlocking",
+        "downloads",
         "*://manaba.tsukuba.ac.jp/*",
       ],
       options_ui: {

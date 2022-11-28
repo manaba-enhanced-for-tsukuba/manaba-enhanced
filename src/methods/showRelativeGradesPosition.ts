43e631882ab2c5f8b3ca28d3eb9e4d1ee0e1386a
Yuzuki Arai <yudukikun5120@gmail.com>
2022-11-28 21:13:21 +0900

    chore: reduce complexity

--- src/methods/showRelativeGradesPosition.ts
+++ src/methods/showRelativeGradesPosition.ts
@@ -2,19 +2,13 @@ const showRelativeGradesPosition = () =>
   document.querySelectorAll<HTMLElement>(".gradebar").forEach((gradebar) => {
     const barWidth = gradebar.getAttribute("width")
     const barSpanElement = gradebar.querySelector<HTMLElement>("span")
+    const siblingBarWidth = gradebar.nextElementSibling?.getAttribute("width")
     if (!barWidth || !barSpanElement) return
 
+    const floorRate = siblingBarWidth ? parseInt(siblingBarWidth) : 0
     const rate = parseInt(barWidth)
 
-    if (gradebar.nextElementSibling === null) {
-      barSpanElement.textContent = `0 - ${rate}%`
-    } else {
-      const siblingBarWidth = gradebar.nextElementSibling.getAttribute("width")
-      if (!siblingBarWidth) return
-
-      const higherRate = parseInt(siblingBarWidth)
-      barSpanElement.textContent = `${higherRate} - ${rate + higherRate}%`
-    }
+    barSpanElement.textContent = `${floorRate} - ${floorRate + rate}%`
   })
 
 export { showRelativeGradesPosition }

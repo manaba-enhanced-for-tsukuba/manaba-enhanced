b89e5001b2c3bb155064c162085ebe3eb3bad787
Yuzuki Arai <arai@yudukikun5120.me>
2022-12-11 03:42:56 +0900

    fix: #520 (#524)

--- src/methods/filterCourses.ts
+++ src/methods/filterCourses.ts
@@ -7,6 +7,23 @@ import { checkLang } from "./checkLang"
 
 let lang: checkLang.langCode
 
+type ViewMode = "list" | "thumbnail"
+
+const translations = {
+  ja: {
+    spring: "春",
+    autumn: "秋",
+    allModules: "すべてのモジュール",
+    courseInfoRegex: /^([春秋])([abc]+)/i,
+  },
+  en: {
+    spring: "Spring",
+    autumn: "Autumn",
+    allModules: "All modules",
+    courseInfoRegex: /^(Spring|Autumn)\s([abc]+)/i,
+  },
+}
+
 export const filterCourses = (): void => {
   lang = checkLang()
 
@@ -44,40 +61,21 @@ export const filterCourses = (): void => {
  */
 const parseModuleCode = (
   moduleCode: ModuleCode
-): { season: SeasonCode; module: ModuleCode } => {
-  const season = moduleCode.split("-")[0] as SeasonCode
-  const module = moduleCode.split("-")[1] as ModuleCode
-
-  return { season, module }
-}
+): { season: SeasonCode; module: ModuleCode } => ({
+  season: moduleCode.split("-")[0] as SeasonCode,
+  module: moduleCode.split("-")[1] as ModuleCode,
+})
 
 /**
  * Convert season code to the text for UI according to the display language
+ * @param {string} lang
  * @param {string} seasonCode "spring" or "autumn"
  * @return {string} "春", "Spring", etc...
  */
-const seasonCodeToText = (seasonCode: SeasonCode) => {
-  switch (seasonCode) {
-    case "spring": {
-      if (lang === "ja") {
-        return "春"
-      } else if (lang === "en") {
-        return "Spring"
-      } else {
-        return ""
-      }
-    }
-    case "autumn": {
-      if (lang === "ja") {
-        return "秋"
-      } else if (lang === "en") {
-        return "Autumn"
-      } else {
-        return ""
-      }
-    }
-  }
-}
+const seasonCodeToText = (
+  lang: checkLang.langCode,
+  seasonCode: SeasonCode
+): string => translations[lang][seasonCode]
 
 const createModuleSelector = () => {
   const selectorsContainer = document.querySelector(
@@ -97,188 +95,137 @@ const createModuleSelector = () => {
     "autumn-c",
   ]
 
-  const moduleCodeToText = (moduleCode: ModuleCode) => {
-    if (moduleCode === "all") {
-      if (lang === "ja") {
-        return "すべてのモジュール"
-      } else if (lang === "en") {
-        return "All modules"
-      }
-    }
-
-    const parsedModuleCode = parseModuleCode(moduleCode)
-
-    const season = seasonCodeToText(parsedModuleCode.season)
-
-    return `${season}${parsedModuleCode.module.toUpperCase()}`
-  }
-
   moduleCodes.forEach((moduleCode: ModuleCode) => {
     const optionDom = document.createElement("option")
     optionDom.value = moduleCode
     optionDom.innerText = moduleCodeToText(moduleCode)
-    if (moduleCode === "all") {
-      optionDom.setAttribute("selected", "true")
-    }
+    if (moduleCode === "all") optionDom.setAttribute("selected", "true")
 
     moduleSelector.appendChild(optionDom)
   })
 
-  if (selectorsContainer) {
+  if (selectorsContainer)
     selectorsContainer.insertBefore(
       moduleSelector,
       selectorsContainer.childNodes[0]
     )
-  }
 
   return moduleSelector
 }
 
-const applyFilter = (moduleCode: ModuleCode): void => {
-  let viewMode: "list" | "thumbnail"
-  let courses: HTMLElement[]
+const getViewModeQuery = () =>
+  document
+    .querySelector<HTMLAnchorElement>(".my-infolist-mycourses .current a")
+    ?.href.split("?")
+    .pop()
 
-  const coursesListContainer = document.querySelector(".courselist tbody")
-  const coursesThumbnailContainer = document.querySelector(
+const applyFilter = (moduleCode: ModuleCode): void => {
+  const coursesListContainer =
+    document.querySelector<HTMLElement>(".courselist tbody")
+  const coursesThumbnailContainer = document.querySelector<HTMLElement>(
     ".mycourses-body .section"
   )
+  const coursesContainer = coursesListContainer ?? coursesThumbnailContainer
+  const viewMode = new URLSearchParams(getViewModeQuery()).get(
+    "chglistformat"
+  ) as ViewMode | null
 
-  if (coursesListContainer) {
-    viewMode = "list"
+  if (!coursesContainer || !viewMode) return
 
-    courses = Array.from(coursesListContainer.children) as HTMLElement[]
-    courses.shift()
-  } else if (coursesThumbnailContainer) {
-    viewMode = "thumbnail"
-
-    courses = Array.from(coursesThumbnailContainer.children) as HTMLElement[]
-    courses.pop()
-  } else {
-    throw "invalid viewMode"
-  }
-
-  /**
-   * Parse course info string on the UI
-   * @param {string} courseInfoString Something like "秋A 水5,6" or "Spring AB Mon. 2"
-   * @return {{ season: Object.<string, boolean>, module: Array.<string>, dayOfWeek: Object.<string, boolean>, period: Array.<string>}}
-   */
-  const parseCourseInfoString = (
-    courseInfoString: string
-  ): {
-    season: { [key in SeasonCode]: boolean }
-    module: string[]
-  } | void => {
-    if (lang === "ja") {
-      const courseInfoRegex = /^([春秋])([abc]+)/i
-
-      if (courseInfoRegex.test(courseInfoString)) {
-        const match = courseInfoString.match(courseInfoRegex)
-
-        if (match) {
-          const [, season, module] = match
-
-          return {
-            season: {
-              spring: season.includes("春"),
-              autumn: season.includes("秋"),
-            },
-            module: module.split("").map((str) => str.toLowerCase()),
-          }
-        }
-      }
-    } else if (lang === "en") {
-      const courseInfoRegex = /^(Spring|Autumn)\s([abc]+)/i
-
-      if (courseInfoRegex.test(courseInfoString)) {
-        const match = courseInfoString.match(courseInfoRegex)
-
-        if (match) {
-          const [, season, module] = match
-
-          return {
-            season: {
-              spring: season === "Spring",
-              autumn: season === "Autumn",
-            },
-            module: module.split("").map((str) => str.toLowerCase()),
-          }
-        }
-      }
-    } else {
-      throw "invalid lang"
-    }
-  }
+  const courses = getCourses(coursesContainer, viewMode)
 
   let isOddRow = true
 
   const handleOddRow = (course: HTMLElement) => {
-    if (isOddRow) {
-      course.classList.replace("row0", "row1")
-    } else {
-      course.classList.replace("row1", "row0")
-    }
+    isOddRow
+      ? course.classList.replace("row0", "row1")
+      : course.classList.replace("row1", "row0")
     isOddRow = !isOddRow
   }
 
-  const showCourse = (course: HTMLElement) => {
-    if (viewMode === "list") {
-      course.style.display = "table-row"
-      handleOddRow(course)
-    } else if (viewMode === "thumbnail") {
-      course.style.display = "block"
-    }
+  const showCourse = (viewMode: ViewMode, course: HTMLElement) => {
+    course.style.display = viewMode === "list" ? "table-row" : "block"
+    if (viewMode === "list") handleOddRow(course)
   }
 
-  const hideCourse = (course: HTMLElement) => {
-    course.style.display = "none"
-  }
+  const hideCourse = (course: HTMLElement) => (course.style.display = "none")
 
   if (moduleCode !== "all") {
     const parsedModuleCode = parseModuleCode(moduleCode)
 
     courses.forEach((course) => {
-      let courseInfoString: string
+      if (viewMode === "list") course.style.display = "table-row"
 
-      if (viewMode === "list") {
-        course.style.display = "table-row"
-      }
-
-      if (viewMode === "list") {
-        courseInfoString = (course.children[2] as HTMLElement)
-          .innerText as string
-      } else if (viewMode === "thumbnail") {
-        const courseInfoStringElm = course.querySelector(
-          ".courseitemdetail-date span"
-        ) as HTMLElement
-
-        if (courseInfoStringElm) {
-          courseInfoString = courseInfoStringElm.title
-        } else {
-          courseInfoString = ""
-        }
-      } else {
-        throw "invalid viewMode"
-      }
+      const courseInfoString = getCourseInfoString(viewMode, course)
 
       if (/^.+\s.+$/.test(courseInfoString)) {
-        const courseInfo = parseCourseInfoString(courseInfoString)
+        const courseInfo = parseCourseInfoString(courseInfoString, lang)
 
-        if (
-          courseInfo &&
-          courseInfo.season[parsedModuleCode.season] &&
-          courseInfo.module.includes(parsedModuleCode.module)
-        ) {
-          showCourse(course)
-        } else {
-          hideCourse(course)
-        }
-      } else {
-        showCourse(course)
-      }
+        courseInfo &&
+        courseInfo.season[parsedModuleCode.season] &&
+        courseInfo.module.includes(parsedModuleCode.module)
+          ? showCourse(viewMode, course)
+          : hideCourse(course)
+      } else showCourse(viewMode, course)
     })
-  } else {
-    courses.forEach((course) => {
-      showCourse(course)
-    })
+  } else courses.forEach((course) => showCourse(viewMode, course))
+}
+
+const getCourses = (coursesContainer: HTMLElement, viewMode: ViewMode) =>
+  (Array.from(coursesContainer.children) as HTMLElement[]).filter(
+    (_, i) =>
+      i !==
+      rowIndexToBeExcluded(viewMode, coursesContainer.childElementCount - 1)
+  )
+
+const rowIndexToBeExcluded = (viewMode: ViewMode, lastIndex: number) =>
+  viewMode === "list" ? 0 : lastIndex
+
+const getCourseInfoString = (viewmode: ViewMode, course: HTMLElement) => {
+  switch (viewmode) {
+    case "list":
+      return (course.children[2] as HTMLElement).innerText
+    case "thumbnail": {
+      const courseInfoStringElm = course.querySelector<HTMLElement>(
+        ".courseitemdetail-date span"
+      )
+      return courseInfoStringElm ? courseInfoStringElm.title : ""
+    }
   }
+}
+
+/**
+ * Parse course info string on the UI
+ * @param {string} courseInfoString Something like "秋A 水5,6" or "Spring AB Mon. 2"
+ * @return {{ season: Object.<string, boolean>, module: Array.<string>, dayOfWeek: Object.<string, boolean>, period: Array.<string>}}
+ */
+const parseCourseInfoString = (
+  courseInfoString: string,
+  lang: checkLang.langCode
+): {
+  season: { [key in SeasonCode]: boolean }
+  module: string[]
+} | void => {
+  const courseInfoRegex = translations[lang].courseInfoRegex
+  const match = courseInfoString.match(courseInfoRegex)
+  if (!courseInfoRegex.test(courseInfoString) || !match) return
+
+  const [, season, module] = match
+
+  return {
+    season: {
+      spring: season.includes(translations[lang].spring),
+      autumn: season.includes(translations[lang].autumn),
+    },
+    module: module.split("").map((str) => str.toLowerCase()),
+  }
+}
+
+const moduleCodeToText = (moduleCode: ModuleCode) => {
+  if (moduleCode === "all") return translations[lang].allModules
+
+  const parsedModuleCode = parseModuleCode(moduleCode)
+  const season = seasonCodeToText(lang, parsedModuleCode.season)
+
+  return `${season}${parsedModuleCode.module.toUpperCase()}`
 }

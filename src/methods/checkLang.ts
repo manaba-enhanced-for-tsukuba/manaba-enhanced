b2f4dc5ed3c61300fa56131990fbd9f68a4b2b12
Yuzuki Arai <yudukikun5120@gmail.com>
2022-12-14 07:33:08 +0900

    fix: great requested changes by @mkobayashime

--- src/methods/checkLang.ts
+++ src/methods/checkLang.ts
@@ -14,17 +14,8 @@ export const checkLang = (): checkLang.langCode => {
 const checkLangByDOMElement = (
   mylang: HTMLElement
 ): checkLang.langCode | undefined => {
-  switch (mylang.className) {
-    case "mylang-ja":
-      return "ja"
-    case "mylang-en":
-      return "en"
-  }
-
-  switch (mylang.innerText) {
-    case "日本語":
-      return "ja"
-    case "English":
-      return "en"
-  }
+  if (mylang.classList.contains("mylang-ja")) return "ja"
+  if (mylang.classList.contains("mylang-en")) return "en"
+  if (mylang.innerText.includes("日本語")) return "ja"
+  if (mylang.innerText.includes("English")) return "en"
 }

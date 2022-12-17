9f91ccd7720f5ed0e630693f8167984739366484
Yuzuki Arai <yudukikun5120@gmail.com>
2022-12-17 18:18:55 +0900

    refactor: complexity

--- src/methods/colorizeDeadline.ts
+++ src/methods/colorizeDeadline.ts
@@ -1,52 +1,64 @@
-"use strict"
-
-import dayjs from "dayjs"
+import dayjs, { Dayjs } from "dayjs"
 import customParseFormat from "dayjs/plugin/customParseFormat"
+
+import { checkLang } from "./checkLang"
 
 dayjs.extend(customParseFormat)
 
-const colorizeDeadline = ({
-  checkStatus = false,
-}: {
-  checkStatus?: boolean
-}): void => {
-  const now = dayjs()
-
-  const rows = Array.from(
-    document.querySelectorAll(".row0, .row1, .row")
-  ) as HTMLElement[]
-
-  const evalDeadline = (row: HTMLElement) => {
-    const deadline = (row.childNodes[row.childNodes.length - 2] as HTMLElement)
-      .innerHTML
-    if (deadline) {
-      const target = dayjs(deadline, "YYYY-MM-DD HH:mm")
-      const diffDays = target.diff(now, "day")
-
-      if (diffDays < 1) {
-        row.classList.add("one-day-before")
-      } else if (diffDays < 3) {
-        row.classList.add("three-days-before")
-      } else if (diffDays < 7) {
-        row.classList.add("seven-days-before")
-      }
-    }
-  }
-
-  for (const row of rows) {
-    if (checkStatus) {
-      const status = (row.childNodes[row.childNodes.length - 6] as HTMLElement)
-        .innerHTML
-      if (
-        (status.includes("未提出") && !status.includes("受付終了")) ||
-        (status.includes("Not submitted") && !status.includes("Closed"))
-      ) {
-        evalDeadline(row)
-      }
-    } else {
-      evalDeadline(row)
-    }
-  }
+const trans = {
+  ja: {
+    notSubmitted: "未提出",
+    closed: "受付終了",
+  },
+  en: {
+    notSubmitted: "Not submitted",
+    closed: "Closed",
+  },
 }
+
+const colorizeDeadline = ({ checkStatus = false }): void => {
+  const now = dayjs()
+  const lang = checkLang()
+
+  Array.from<HTMLElement>(assignmentRowElements(document))
+    .map((row) => ({
+      row,
+      deadline: getDeadline(row),
+      status: checkStatus ? getStatus(row) : null,
+    }))
+    .filter(({ deadline }) => deadline)
+    .filter(
+      ({ status }) =>
+        !status ||
+        (status &&
+          status.includes(trans[lang].notSubmitted) &&
+          !status.includes(trans[lang].closed))
+    )
+    .forEach(({ row, deadline }) => addClassNameToRow(row, deadline, now))
+}
+
+const assignmentRowElements = (document: Document) =>
+  document.querySelectorAll<HTMLElement>(".row0, .row1, .row")
+
+const getDeadline = (row: HTMLElement) =>
+  (row.childNodes[row.childNodes.length - 2] as HTMLElement).innerHTML
+
+const getStatus = (row: HTMLElement) =>
+  (row.childNodes[row.childNodes.length - 6] as HTMLElement).innerHTML
+
+const addClassNameToRow = (row: HTMLElement, deadline: string, now: Dayjs) => {
+  const target = dayjs(deadline, "YYYY-MM-DD HH:mm")
+  const className = classNameFromDiffDays(target.diff(now, "day"))
+  if (className) row.classList.add(className)
+}
+
+const classNameFromDiffDays = (diffDays: number): string | null =>
+  diffDays < 1
+    ? "one-day-before"
+    : diffDays < 3
+    ? "three-days-before"
+    : diffDays < 7
+    ? "seven-days-before"
+    : null
 
 export default colorizeDeadline

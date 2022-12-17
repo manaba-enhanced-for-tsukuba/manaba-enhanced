33bc7eb27f10fedbfc1ec94b46b40f79135f1841
Yuzuki Arai <yudukikun5120@gmail.com>
2022-12-17 17:15:10 +0900

    fix: `checkLangElement` in `checkLang`

--- src/methods/checkLang.ts
+++ src/methods/checkLang.ts
@@ -4,20 +4,19 @@ declare namespace checkLang {
   type langCode = "ja" | "en"
 }
 
-const getCheckLangElement = () => document.getElementById("mylang")
-
 const checkLangByDOMElement = (
   mylang: HTMLElement
 ): checkLang.langCode | undefined => {
   if (mylang.classList.contains("mylang-ja")) return "ja"
   if (mylang.classList.contains("mylang-en")) return "en"
+  /* The fallback for the case where the service changes the class name */
   if (mylang.innerText.includes("日本語")) return "ja"
   if (mylang.innerText.includes("English")) return "en"
 }
 
-const checkLang = (
-  checkLangElement: HTMLElement | null = getCheckLangElement()
-): checkLang.langCode =>
-  (checkLangElement && checkLangByDOMElement(checkLangElement)) ?? "ja"
+const checkLang = (): checkLang.langCode => {
+  const checkLangElement = document.getElementById("mylang")
+  return (checkLangElement && checkLangByDOMElement(checkLangElement)) ?? "ja"
+}
 
 export { checkLang }
